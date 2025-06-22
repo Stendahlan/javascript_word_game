ef596e66c75c9bfda7cabda1ba916c3b53820fdd
Andre Stendahl <andrestendahl@10-197-145-92.sds.oregonstate.edu>
2025-06-22 15:04:04 -0700

    Initial commit
    Create basic files and put in 'hello world' text

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-blue-50">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello World
+        </h1>
+      </div>
+    )
+  }
+  
+  export default App
